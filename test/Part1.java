fb83caf635d7b91978a960e89f3ffbe945181893
petitghost <ghost@ibms.sinica.edu.tw>
2014-05-19 11:25:45 +0800

    start of second test case

--- test/Part1.java
+++ test/Part1.java
@@ -6,4 +6,20 @@ public class Part1 extends TestCase {
 		assertEquals("", sheet.get("A1"));
 		assertEquals("", sheet.get("ZX347"));
 	}
+
+	// Implement each test before going to the next one.
+
+	public void testThatTextCellsAreStored() {
+	  Sheet sheet = new Sheet();
+	  String theCell = "A21";
+
+	  sheet.put(theCell, "A string");
+	  assertEquals("A string", sheet.get(theCell));
+
+	  sheet.put(theCell, "A different string");
+	  assertEquals("A different string", sheet.get(theCell));
+
+	  sheet.put(theCell, "");
+	  assertEquals("", sheet.get(theCell));
+	}
 }
